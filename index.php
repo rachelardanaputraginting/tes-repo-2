fdc5b5bc711b9f8151c03ebd2fce2af5118241ab
Rachel Ardana Putra Ginting <rachelardanaputraginting>
2022-08-02 15:41:08 +0700

    Menambahkan daftar

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi culpa eius recusandae quaerat quae aut. Sit illum amet deleniti totam nemo, itaque quas vero minima dolore enim sapiente accusamus! Non deserunt sit cum illum exercitationem. Modi illo veritatis vero architecto laudantium autem! Quod, reiciendis molestias voluptate optio illo provident debitis ratione cupiditate quia quis odio sint nostrum accusantium voluptatibus hic officiis suscipit corrupti facere corporis consequuntur rem magni? Laborum quae cum pariatur quia velit natus nemo commodi, voluptatibus, quidem nobis deleniti suscipit consectetur aut facilis nisi libero amet beatae! Placeat officiis sed ullam id nobis reiciendis repudiandae earum maiores ad?</p>
+    <ul>
+        <li>Rachel Ginting</li>
+        <li>Ardana Putra</li>
+    </ul>
 </body>
 </html>
